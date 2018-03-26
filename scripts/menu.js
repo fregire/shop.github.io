dc73c467d6fc89717e970a00757081de3f412ce5
= <=>
2018-03-26 16:08:50 +0600

    Added paddings for wrappers

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,7 +2,9 @@
 
 	var menu     = document.querySelector('.js-menu'),
 		headMenu = document.querySelector('.header__mobile-nav');
-	headMenu.classList.remove('header__mobile-nav--nojs');
+	// If js scripts dont work, then it wont work
+	headMenu.classList.remove('header__mobile-nav--nojs'); 
+
 	menu.addEventListener('click', function() {
 		document.querySelector('.nav--mobile').classList.toggle('nav--mobile_active');
 	});
